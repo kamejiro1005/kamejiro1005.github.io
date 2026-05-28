81e9ab07eaf9179048619fde5444305a77aad5ea
kamejiro <kamejiro1005@gmail.com>
2026-05-28 10:14:09 +0900

    astro.config.mjsに書き足しました。

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,11 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    // 1. 公開先のURL（自分のGitHubのURL）
+    site: 'https://kamejiro1005.github.io/.github.io/', 
+
+    // 2. リポジトリ名（例：my-astro-site）
+    // これを書かないと、画像やスタイルが読み込まれず真っ白になります！
+    base: '/.github.io',
+});
